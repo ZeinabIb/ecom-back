ea0dcf6d57819403b58b2ede34ac28be21296e39
eliebc <eliebc1999@gmail.com>
2024-04-14 14:00:59 +0300

    process

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -4,5 +4,4 @@
 
         </x-app-layout>
 
-
 	</header>
--- routes/web.php
+++ routes/web.php
@@ -77,7 +77,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/', function () {
+        return view('home.home');
+    })->name('home.home');
 });
